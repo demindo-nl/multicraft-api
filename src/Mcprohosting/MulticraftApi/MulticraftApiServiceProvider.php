728d7c33ce268e61ed26096f76e8713863d6c274
Connor Peet <connor@connorpeet.com>
2014-05-23 22:13:39 -0500

    Fixes issue with wonky package config name.

--- src/Mcprohosting/MulticraftApi/MulticraftApiServiceProvider.php
+++ src/Mcprohosting/MulticraftApi/MulticraftApiServiceProvider.php
@@ -2,14 +2,15 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class MulticraftApiServiceProvider extends ServiceProvider {
+class MulticraftApiServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = true;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
     /**
      * Bootstrap the application events.
@@ -18,30 +19,30 @@ class MulticraftApiServiceProvider extends ServiceProvider {
      */
     public function boot()
     {
-        $this->package('mcprohosting/multicraftapi');
+        $this->package('mcprohosting/multicraft-api');
     }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-        $this->app->bind('multicraftapi', function()
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('multicraftapi', function ()
         {
-            return new MulticraftApi($this->app['config']->get('multicraftapi::credentials'));
+            return new MulticraftApi($this->app['config']->get('multicraft-api::credentials'));
         });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('multicraftapi');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('multicraftapi');
+    }
 
 }
